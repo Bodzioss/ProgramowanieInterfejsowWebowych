d73c02e8fbccd24b4a66318e686ebc5818367a6c
Bodzioss <marcin_bogdanski1@wp.pl>
2022-04-24 23:29:14 +0200

    Create react project

---
+++ Lab3/reactapp/src/Header.js
@@ -0,0 +1,7 @@
+import React, {PureComponent} from "react";
+
+export default class Header extends PureComponent{
+    render() {
+        return <h1>Aplikacja do szukania nowych grup projektowych lub członków do już istniejących grup </h1>
+    }
+} 
